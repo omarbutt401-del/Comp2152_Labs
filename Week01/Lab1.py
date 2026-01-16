2f91e166faa545778c0b9fa057ea7fd3e68213e3
omarbutt401-del <omar.butt401@gmail.com>
2026-01-16 11:52:33 -0500

    Week01

---
+++ Week01/Lab1.py
@@ -0,0 +1,5 @@
+# sample : coding questions: 01 Week 01
+#Name : Omar Butt
+
+# Question 2: Defining a array(list)
+my_array = [1,4,7,9]
---
+++ Week01/q5.py
@@ -0,0 +1,3 @@
+user_age = int(input("Enter your age: "))
+user_age += 22
+print("Now showing the shop items filtered for age:", user_age)
